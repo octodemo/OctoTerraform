24245fc4371af9cf64b39b7cae2fe8dd134f3d3e
Alain Hélaïli <alain@carbonhub.io>
2021-05-20 17:00:02 +0200

    dynamically get the AMI

--- main.tf
+++ main.tf
@@ -11,12 +11,28 @@ terraform {
 
 provider "aws" {
   profile = "default"
-  region  = "us-west-2"
+  region  = "eu-west-3"
 }
 
+data "aws_ami" "latest-ubuntu" {
+most_recent = true
+owners = ["099720109477"] # Canonical
+
+  filter {
+      name   = "name"
+      values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
+  }
+
+  filter {
+      name   = "virtualization-type"
+      values = ["hvm"]
+  }
+}
+
+
 resource "aws_instance" "app_server" {
-  ami           = "ami-0d5eff06f840b45e9"
-  instance_type = "t2.micro"
+  ami           = "${data.aws_ami.latest-ubuntu.id}"
+  instance_type = "t2.nano"
 
   tags = {
     Name      = "ExampleAppServerInstance"
