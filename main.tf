5914cbb04bb83e660a90f9157a68e4b054cd0bda
Alain Hélaïli <alain@carbonhub.io>
2021-05-21 12:16:46 +0200

    Revert back to dynamic AMI

--- main.tf
+++ main.tf
@@ -14,8 +14,24 @@ provider "aws" {
   region  = "eu-west-3"
 }
 
+data "aws_ami" "latest-ubuntu" {
+most_recent = true
+owners = ["099720109477"] # Canonical
+
+  filter {
+      name   = "name"
+      values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
+  }
+
+  filter {
+      name   = "virtualization-type"
+      values = ["hvm"]
+  }
+}
+
+
 resource "aws_instance" "app_server" {
-  ami           = "ami-0b3e57ee3b63dd76b"
+  ami           = "${data.aws_ami.latest-ubuntu.id}"
   instance_type = "t2.nano"
 
   tags = {
